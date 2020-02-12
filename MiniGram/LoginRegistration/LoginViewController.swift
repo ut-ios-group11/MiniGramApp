4092edf49b538dbb4fe30db51db43c256811fd36
Keegan Black <keeganblack@utexas.edu>
2020-02-12 16:58:40 -0600

    added basic sign in

---
+++ MiniGram/LoginRegistration/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  MiniGram
+//
+//  Created by Keegan Black on 2/12/20.
+//  Copyright © 2020 Keegan Black. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginOutputLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    @IBAction func loginClick(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+            LogManager.logError("No Email/Password")
+            return
+        }
+        
+        UserData.shared.signIn(email: email, password: password, onError: { (error) in
+            LogManager.logError(error)
+            self.loginOutputLabel.text = "Login Error"
+        }, onComplete: {
+            LogManager.logInfo("Login Succesful")
+            self.loginOutputLabel.text = "Login Success"
+        })
+    }
+    
+    @IBAction func skipClick(_ sender: Any) {
+        performSegue(withIdentifier: "toSignedIn", sender: self)
+    }
+
+}
---
+++ MiniGram/SimpleExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  SimpleExtensions.swift
+//  MiniGram
+//
+//  Created by Keegan Black on 2/12/20.
+//  Copyright © 2020 Keegan Black. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
