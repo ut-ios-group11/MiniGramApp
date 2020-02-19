d71f6266baffd5d9af61cb92717ce143e5127aad
Matthew Ewing <matthew.ewing@utexas.edu>
2020-02-19 11:40:24 -0600

    Fully tested account creation. Everything populates in the DB.

--- MiniGram/LoginRegistration/CreateAccountViewController.swift
+++ MiniGram/LoginRegistration/CreateAccountViewController.swift
@@ -13,16 +13,16 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
-
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var statusLabel: UILabel!
 
     @IBAction func submitPressed(_ sender: Any) {
-        guard usernameTextField.text!.isEmpty || passwordTextField.text!.isEmpty || firstNameTextField.text!.isEmpty || lastNameTextField.text!.isEmpty else {
+        guard !usernameTextField.text!.isEmpty || !emailTextField.text!.isEmpty || !passwordTextField.text!.isEmpty || !firstNameTextField.text!.isEmpty || !lastNameTextField.text!.isEmpty else {
             statusLabel.text = "Ensure all fields are filled out."
             return
         }
-        Database.shared.createUser(email: "", password: passwordTextField.text!, username: usernameTextField.text!, firstName: firstNameTextField.text!, lastName: lastNameTextField.text!, onError: { (error) in
+        Database.shared.createUser(email: emailTextField.text!, password: passwordTextField.text!, username: usernameTextField.text!, firstName: firstNameTextField.text!, lastName: lastNameTextField.text!, onError: { (error) in
             LogManager.logError("Create User Field not filled out")
         }) {
             // try auto login here
