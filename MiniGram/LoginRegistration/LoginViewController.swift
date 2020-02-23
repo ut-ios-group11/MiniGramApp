6b34ac9db7dd5f8f5c065acb4d7d70ad944bc9f9
Keegan Black <keeganblack@utexas.edu>
2020-02-22 23:20:41 -0600

    added spinner to login screen

--- MiniGram/LoginRegistration/LoginViewController.swift
+++ MiniGram/LoginRegistration/LoginViewController.swift
@@ -14,10 +14,15 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var loginOutputLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        spinner.hidesWhenStopped = true
+        spinner.roundCorners(4)
     }
     
     @IBAction func loginClick(_ sender: Any) {
@@ -26,9 +31,11 @@ class LoginViewController: UIViewController {
             return
         }
         
+        disableInteraction(true)
         UserData.shared.signIn(email: email, password: password, onError: { (error) in
             LogManager.logError(error)
             self.loginOutputLabel.text = "Login Error"
+            self.disableInteraction(false)
         }, onComplete: {
             LogManager.logInfo("Login Succesful")
             if let name = UserData.shared.getDatabaseUser()?.name {
@@ -36,7 +43,23 @@ class LoginViewController: UIViewController {
             } else {
                 self.loginOutputLabel.text = "NO NAME was logged in"
             }
+            self.disableInteraction(false)
         })
+    }
+    
+    func disableInteraction(_ bool: Bool) {
+        let userInteraction = !bool
+        
+        emailTextField.isUserInteractionEnabled = userInteraction
+        passwordTextField.isUserInteractionEnabled = userInteraction
+        loginButton.isUserInteractionEnabled = userInteraction
+        createAccountButton.isUserInteractionEnabled = userInteraction
+        
+        if bool {
+            spinner.startAnimating()
+        } else {
+            spinner.stopAnimating()
+        }
     }
     
     @IBAction func skipClick(_ sender: Any) {
