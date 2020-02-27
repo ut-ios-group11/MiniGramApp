629d088289c0043c098f71430a20d4a7b71f56fa
Keegan Black <keeganblack@utexas.edu>
2020-02-27 13:34:37 -0600

    grid collection view layout

--- MiniGram/Explore/ViewControllers/ExploreViewController.swift
+++ MiniGram/Explore/ViewControllers/ExploreViewController.swift
@@ -11,13 +11,30 @@ import UIKit
 class ExploreViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
-
+    let collectionViewXInset: CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.reloadData()
         // Do any additional setup after loading the view.
+        
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: collectionViewXInset, bottom: 0, right: collectionViewXInset)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        collectionView!.collectionViewLayout = layout
+        
+        collectionView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
 
@@ -25,10 +42,8 @@ extension ExploreViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let numberOfColumns: CGFloat = 2
         let width = collectionView.frame.size.width
-        let xInsets: CGFloat = 10
-        let cellSpacing: CGFloat = 5
-        
-        return CGSize(width: (width / numberOfColumns) - (xInsets + cellSpacing), height: (width / numberOfColumns) - (xInsets + cellSpacing))
+        let xInsets = collectionViewXInset * 2
+        return CGSize(width: ((width - xInsets) / numberOfColumns), height: ((width - xInsets) / numberOfColumns))
     }
 }
 
