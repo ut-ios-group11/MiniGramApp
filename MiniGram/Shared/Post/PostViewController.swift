ced905c41c63aa1dc1df3b348d02780b8e2ca3ca
Keegan Black <keeganblack@utexas.edu>
2020-03-02 09:41:06 -0600

    added generic post storyboard

---
+++ MiniGram/Shared/Post/PostViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PostViewController.swift
+//  MiniGram
+//
+//  Created by Keegan Black on 2/28/20.
+//  Copyright © 2020 Keegan Black. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var descTextView: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+    
+    var post: GenericPost?
+    var user: GenericUser?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userImage.round()
+        refreshData()
+        // Do any additional setup after loading the view.
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func refreshData() {
+        if let post = post {
+            postImage.image = post.image
+            likesLabel.text = "\(post.likes) likes"
+            descTextView.text = post.desc
+        }
+        if let user = user {
+            usernameLabel.text = user.name
+            userImage.image = user.image
+        }
+    }
+    
+    @IBAction func likeClick(_ sender: UIButton) {
+        // Update Database instead of local.
+        if (!sender.isSelected) {
+            sender.isSelected = true
+            post?.likes += 1
+            refreshData()
+        } else {
+            sender.isSelected = false
+            post?.likes -= 1
+            refreshData()
+        }
+        
+    }
+
+}
