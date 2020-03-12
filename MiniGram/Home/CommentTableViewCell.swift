58ce93b78d2a931c8550cddc314c20feacc1263c
Matthew Ewing <matthew.ewing@utexas.edu>
2020-03-12 16:56:12 -0500

    Comment View WIP

---
+++ MiniGram/Home/CommentTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CommentTableViewCell.swift
+//  MiniGram
+//
+//  Created by Matthew Ewing on 3/9/20.
+//  Copyright © 2020 Keegan Black. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    
+
+}
