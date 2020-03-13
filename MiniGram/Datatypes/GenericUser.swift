24a4447b02939695d64c33ae2935f189de582669
Keegan Black <keeganblack@utexas.edu>
2020-03-12 20:40:43 -0500

    fixed the blue folders and fixed some compile errors

--- MiniGram/Datatypes/GenericUser.swift
+++ MiniGram/Datatypes/GenericUser.swift
@@ -14,6 +14,7 @@ class GenericUser: FireInitable {
     var id: String
     var name: String?
     var followers: [String]?
+    var image: UIImage?
     
     required init(doc: DocumentSnapshot) {
         id = doc.documentID
