bcb7e2a2c6dda65562f3a1c5a4bea31445123f63
Joseph Manahan <Joseph.Manahan@utexas.edu>
2020-04-09 20:10:42 -0500

    Return Home Segue (WIP)

---
+++ MiniGram/Add/ViewControllers/SubmitPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubmitPostViewController.swift
+//  MiniGram
+//
+//  Created by Joseph Manahan on 4/8/20.
+//  Copyright © 2020 Keegan Black. All rights reserved.
+//
+
+import UIKit
+
+class SubmitPostViewController: UIViewController {
+    
+    @IBOutlet weak var captionField: UITextField!
+    
+    @IBAction func submitButton(_ sender: Any) {
+    }
+}
