44ef122d6b17e925ceb24e28b47220aefccbaab9
Joseph Manahan <Joseph.Manahan@utexas.edu>
2020-04-10 02:37:13 -0500

    Animated captionField

--- MiniGram/Add/ViewControllers/SubmitPostViewController.swift
+++ MiniGram/Add/ViewControllers/SubmitPostViewController.swift
@@ -11,9 +11,63 @@ import UIKit
 class SubmitPostViewController: UIViewController {
     
     @IBOutlet weak var captionField: UITextField!
+    @IBOutlet weak var captionFieldConstraint: NSLayoutConstraint!
     
     @IBAction func submitButton(_ sender: Any) {
         self.performSegue(withIdentifier: "HomeSegue", sender: self)
+    }
+    
+    let CAPTION_FIELD_CONSTRAINT_CONSTANT = 4
+    
+    var captionFieldContraintOriginalValue: CGFloat?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        captionFieldContraintOriginalValue = self.captionFieldConstraint.constant
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let newHeight: CGFloat
+            let duration:TimeInterval = (notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
+            if #available(iOS 11.0, *) {
+                newHeight = keyboardFrame.cgRectValue.height - self.view.safeAreaInsets.bottom
+            } else {
+                newHeight = keyboardFrame.cgRectValue.height
+            }
+            let keyboardHeight = newHeight
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: {
+                            self.captionFieldConstraint.constant = keyboardHeight},
+                           completion: nil)
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        let newHeight = self.captionFieldContraintOriginalValue ?? CGFloat(CAPTION_FIELD_CONSTRAINT_CONSTANT)
+        let duration:TimeInterval = (notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+        let animationCurveRawNSN = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber
+        let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
+        let animationCurve:UIView.AnimationOptions = UIView.AnimationOptions(rawValue: animationCurveRaw)
+        UIView.animate(withDuration: duration,
+                        delay: TimeInterval(0),
+                        options: animationCurve,
+                        animations: {
+                        self.captionFieldConstraint.constant = newHeight},
+                        completion: nil)
     }
     
     func textFieldShouldReturn(textField:UITextField) -> Bool {
